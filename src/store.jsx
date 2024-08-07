8dbb6c8162f3581391e9e5c3ed6f8b410c8cc15b
sharma-dikshant <dikshantsharma2005@gmail.com>
2024-08-07 08:47:40 +0530

    Legacy redux : created customer reducer, action creator functions and combining both account and customer reducer to a single rootReducer

--- src/store.jsx
+++ src/store.jsx
@@ -1,11 +1,17 @@
-import { createStore } from "redux";
-const initialState = {
+import { combineReducers, createStore } from "redux";
+const initialStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
 };
 
-function reducer(state = initialState, action) {
+const initialStateCustomer = {
+  fullname: "",
+  nationalID: "",
+  createdAt: "",
+};
+
+function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
     case "account/deposit":
       return { ...state, balance: state.balance + action.payload };
@@ -31,7 +37,29 @@ function reducer(state = initialState, action) {
   }
 }
 
-const store = createStore(reducer);
+function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case "customer/createAccount":
+      return {
+        ...state,
+        fullname: action.payload.fullname,
+        nationalID: action.payload.nationalID,
+        createdAt: action.payload.createdAt,
+      };
+    case "customer/updateName":
+      return { ...state, fullname: action.payload };
+    default:
+      return state;
+  }
+}
+
+// combine reducers to a root reducer
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
+});
+
+const store = createStore(rootReducer);
 
 console.log("hii redux");
 // store.dispatch({ type: "account/deposit", payload: 500 });
@@ -59,4 +87,23 @@ function payLoan() {
 store.dispatch(deposit(500));
 console.log(store.getState());
 store.dispatch(withdraw(200));
+console.log(store.getState());
+
+//! action creator functions for customer
+
+function createAccount(fullname, nationalID) {
+  return {
+    type: "customer/createAccount",
+    payload: { fullname, nationalID, createdAt: new Date().toISOString() },
+  };
+}
+
+function updateName(fullname) {
+  return {
+    type: "customer/updateName",
+    payload: fullname,
+  };
+}
+
+store.dispatch(createAccount("Dikshant", "123456789"));
 console.log(store.getState());
