e4791672d91647698750a9efcc8f26862f6b3f75
sharma-dikshant <dikshantsharma2005@gmail.com>
2024-08-07 22:17:51 +0530

    update accountSlice using RTK

--- src/features/accounts/AccountOperations.jsx
+++ src/features/accounts/AccountOperations.jsx
@@ -19,7 +19,9 @@ function AccountOperations() {
 
   function handleDeposit() {
     if (!depositAmount) return;
-    dispatch(deposit(depositAmount, currency));
+    // TODO
+    // dispatch(deposit(depositAmount, currency));
+    dispatch(deposit(depositAmount));
     setDepositAmount("");
     setCurrency("");
   }
--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -1,10 +1,44 @@
-const initialStateAccount = {
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
   isLoading: false,
 };
 
+const accountSlice = createSlice({
+  name: "account",
+  initialState,
+  reducers: {
+    deposit(state, action) {
+      state.balance += action.payload;
+    },
+    withdraw(state, action) {
+      state.balance -= action.payload;
+    },
+    requestLoan: {
+      prepare(account, purpose) {
+        return { payload: { account, purpose } };
+      },
+      reducer(state, action) {
+        if (state.loan > 0) return;
+        state.loan = action.payload.amount;
+        state.loanPurpose = action.payload.purpose;
+      },
+    },
+    payload(state, action) {
+      state.balance -= state.loan;
+      state.loan = 0;
+      state.loanPurpose = "";
+    },
+  },
+});
+
+console.log(accountSlice);
+export const { deposit, withdraw, requestLoan, payLoan } = accountSlice.actions;
+export default accountSlice.reducer;
+
+/* 
 export default function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
     case "account/deposit":
@@ -64,3 +98,4 @@ export function requestLoan(amount, purpose) {
 export function payLoan() {
   return { type: "account/payLoan" };
 }
+ */
