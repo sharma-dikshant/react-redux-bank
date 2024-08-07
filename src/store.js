11d728ea66a1a0b43f3b4aedfd3036c3da83e966
sharma-dikshant <dikshantsharma2005@gmail.com>
2024-08-07 21:43:54 +0530

    created store using modern method - RTK

--- src/store.js
+++ src/store.js
@@ -1,12 +1,12 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
-import { thunk } from "redux-thunk";
+import { configureStore } from "@reduxjs/toolkit";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
 // combine reducers to a root reducer
-const rootReducer = combineReducers({
-  account: accountReducer,
-  customer: customerReducer,
-});
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    customer: customerReducer,
+  },
+});
 export default store;
